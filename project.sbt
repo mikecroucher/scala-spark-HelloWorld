e2de66515b39cb899518fbf77c27224b5a5cb75c
Mike Croucher <michael.p.croucher@googlemail.com>
2017-02-09 14:42:49 +0000

    First commit

---
+++ project.sbt
@@ -0,0 +1,6 @@
+name := "hello" 
+version := "1.0"
+scalaVersion := "2.11.8"
+libraryDependencies += "org.apache.spark" %% "spark-yarn" % "2.0.1"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.0.1"
+
---
+++ src/main/scala/hello.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.sql.SparkSession
+// define main method (scala entry point)
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+
+    // initialise spark session (running in "local" mode)
+    val sparkSession = SparkSession.builder
+      .master("local")
+      .appName("Hello World")
+       .getOrCreate()
+
+    ///shhhhhhhh spark
+    spark.sparkContext.setLogLevel('ERROR')
+    
+    // do stuff
+    println("Hello, world!")
+    
+    // terminate underlying spark context
+    sparkSession.stop()
+  }
+}
